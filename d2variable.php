5db9fbbe8a2ed857e74682d426c471e7d1018e7d
honeypatel6 <83407113+honeypatel6@users.noreply.github.com>
2021-05-26 21:53:15 +0530

    Add files via upload

---
+++ d2variable.php
@@ -0,0 +1,18 @@
+<?php
+//Variable
+//Metho
+$a = 10;
+$b = 20;
+//Method 1
+echo "A Value is".$a;
+echo "<br/>B Value is".$b;
+//Method 2
+echo "<br/>A Value is ".$a."<br/>B Value is ".$b;
+//Method 3
+echo "<br/>A Value is $a<br/>B Value is $b";
+//Method 4
+echo '<br/>A Value is $a<br/>B Valueis $b';
+//Method 5
+echo "<br/>A Value is $<br/>B Value Is $b Sum is ".($a+$b);
+?>
+
